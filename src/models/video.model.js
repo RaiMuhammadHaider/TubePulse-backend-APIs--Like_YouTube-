cbd2742943e737f4b6376038ec77e970dc582443
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2026-09-10 13:07:19 +0500

    feat: implement models for comments, likes, playlists, tweets, and videos with pagination support

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,39 +1,42 @@
 import mongoose , {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
-const videoSchema = new Schema({
-Video : {
-    type : String,
-    rquired : true,
-},
-thumbnail : {
-    required : true,
-    type : String,
-},
-title : {
-    type : String ,
-    required : true
-},
-description : {
-    type: String,
-    required : true,
-},
-duration : {
-    type : Number, 
-    required : true
-},
-views : {
-    type : Number , 
-    default : 0
-},
-isPublish : {
-    type : Boolean,
-    default : true
-},
-owner : {
-    type : Schema.Types.ObjectId,
-    ref : "User"
-}
 
-}, {timestamps : true})
-videoSchema.plugin(mongooseAggregatePaginate)
-export const Videos = mongoose.model("Video" , videoSchema )
+const videoSchema = new Schema({
+    videoFile : {
+        type : String,
+        required : true
+    },
+    thumbnail : {
+        type : String,
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+
+    },
+    description : {
+        type : String,
+        required : true
+    },duration : {
+        type : Number,
+        required : true
+    },
+    views : {
+        type : Number,
+        default : 0
+    },
+    isPunlished : {
+        type : Boolean,
+        require : true
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+    }
+
+
+} , { timestamps : true} )
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video",videoSchema);
